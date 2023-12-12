a6e78d974e1ccdef10de58383f8e1728469c4122
Greg Woods <greg.woods@moserit.com>
2023-12-12 09:22:09 -0500

    day 12 part 1

---
+++ lib/days/12.rb
@@ -0,0 +1,52 @@
+class Day12
+
+  def part1(input)
+    count = 0
+
+    input.split("\n").each do |row|
+      a, b = row.split
+
+      data = a.chars
+
+      legend = b.split(',').map(&:to_i)
+
+      combos = build_combinations(data, legend)
+
+      count += combos.count do |c|
+        convert_data(c) == legend
+      end
+    end
+
+    count
+  end
+
+  def build_combinations(data, legend, current = [[]])
+    results = []
+
+    current.each do |combo|
+      next_value = data[combo.length]
+
+      if next_value == '?'
+        results << combo.map(&:clone).append('.')
+        results << combo.map(&:clone).append('#')
+      else
+        results << combo.map(&:clone).append(next_value)
+      end
+    end
+
+    if results.first.length == data.length
+      results
+    else
+      build_combinations(data, legend, results)
+    end
+  end
+
+  def convert_data(data)
+    data.join.split(/\.+/).reject(&:empty?).map(&:length)
+  end
+
+  def part2(input)
+    'TODO'
+  end
+
+end
---
+++ test/12_test.rb
@@ -0,0 +1,18 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require_relative '../app'
+
+class TestDay12 < Minitest::Test
+  def setup
+    @data = File.read(File.join(APP_ROOT, 'examples', '12.txt')).rstrip
+    @day = Day12.new
+  end
+
+  def test_part1
+    assert_equal @day.part1(@data), 21
+  end
+
+  def test_part2
+    assert_equal @day.part2(@data), ''
+  end
+end
