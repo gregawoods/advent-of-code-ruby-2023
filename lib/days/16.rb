8982d40b3f9807b92f92ba97f829266c7a750750
Greg Woods <greg.woods@moserit.com>
2023-12-18 22:40:19 -0500

    16 part 1

---
+++ lib/days/16.rb
@@ -0,0 +1,104 @@
+class Day16
+  include Util
+
+  MIRROR_LEFT = '\\'
+  MIRROR_RIGHT = '/'
+  SPLIT_VERT = '|'
+  SPLIT_HORI = '-'
+
+  MOVEMENTS = {
+    up: [0, -1],
+    down: [0, 1],
+    right: [1, 0],
+    left: [-1, 0]
+  }
+
+  Beam = Struct.new(:x, :y, :dir) do
+    def move(dir)
+      mv = MOVEMENTS[dir]
+      Beam.new(x + mv[0], y + mv[1], dir)
+    end
+
+    def pos
+      [x, y]
+    end
+  end
+
+  def part1(input)
+    grid = parse_grid(input)
+    visited = Set.new
+
+    beams = [
+      Beam.new(0, 0, :right)
+    ]
+
+    loop do
+      new_beams = []
+
+      beams.each do |beam|
+        if beam.x.negative? ||
+           beam.x >= grid.length ||
+           beam.y.negative? ||
+           beam.y >= grid.first.length
+          next
+        end
+
+        next if visited.include?(beam)
+
+        visited.add(beam)
+
+        case grid[beam.x][beam.y]
+        when MIRROR_LEFT
+          case beam.dir
+          when :up
+            new_beams << beam.move(:left)
+          when :down
+            new_beams << beam.move(:right)
+          when :left
+            new_beams << beam.move(:up)
+          when :right
+            new_beams << beam.move(:down)
+          end
+        when MIRROR_RIGHT
+          case beam.dir
+          when :up
+            new_beams << beam.move(:right)
+          when :down
+            new_beams << beam.move(:left)
+          when :left
+            new_beams << beam.move(:down)
+          when :right
+            new_beams << beam.move(:up)
+          end
+        when SPLIT_HORI
+          if [:up, :down].include?(beam.dir)
+            new_beams << beam.move(:left)
+            new_beams << beam.move(:right)
+          else
+            new_beams << beam.move(beam.dir)
+          end
+        when SPLIT_VERT
+          if [:left, :right].include?(beam.dir)
+            new_beams << beam.move(:up)
+            new_beams << beam.move(:down)
+          else
+            new_beams << beam.move(beam.dir)
+          end
+        else
+          new_beams << beam.move(beam.dir)
+        end
+      end
+
+      break if new_beams.empty?
+
+      beams = new_beams
+    end
+
+    visited.map(&:pos).uniq.length
+  end
+
+  def part2(input)
+    'TODO'
+  end
+
+end
---
+++ test/16_test.rb
@@ -0,0 +1,18 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require_relative '../app'
+
+class TestDay16 < Minitest::Test
+  def setup
+    @data = File.read(File.join(APP_ROOT, 'examples', '16.txt')).rstrip
+    @day = Day16.new
+  end
+
+  def test_part1
+    assert_equal @day.part1(@data), 46
+  end
+
+  def test_part2
+    assert_equal @day.part2(@data), ''
+  end
+end
